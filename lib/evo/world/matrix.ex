96b3d580760eb6d7c20c47ad12cfb6dcd77ca24d
Brett Wise <brettdwise@gmail.com>
2021-11-24 01:15:17 -0500

    Making s better

--- lib/evo/world/matrix.ex
+++ lib/evo/world/matrix.ex
@@ -75,12 +75,26 @@ defmodule Evo.World.Matrix.Square do
   A `Square` represents a single coordinate point in a `Matrix`. It contains the entities that make up the current
   state of the world.
   """
+  alias Evo.World.Entities.Blarg
+  alias Evo.World.Entities.Vlem
   import Evo.Generators
 
   @doc """
-  Contains ground fertility, any entity if spawned, and space for a reproductive mate if mating.
+  Contains soil fertility rating, blarg or vlem and space for mate when the time comes.
   """
-  def new(entity \\ nil) do
-    {random(5..10), entity, nil}
+  def new(entity \\ nil)
+  def new(%Blarg{} = blarg), do: square(nil, blarg)
+  def new(%Vlem{} = vlem), do: square(vlem, nil)
+  def new(_empty_square), do: square(nil, nil)
+
+  defp square(vlem, blarg) do
+    %{
+      soil_fertility: random_soil_fertility(),
+      vlem: vlem,
+      blarg: blarg,
+      blarg_mate: nil
+    }
   end
+
+  defp random_soil_fertility, do: random(30..70)
 end
