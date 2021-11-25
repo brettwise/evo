c2cd9ee37d9766b94cb086cf21a921f398a988dd
Brett Wise <brettdwise@gmail.com>
2021-11-25 14:15:30 -0500

    More readable in IEX benchmark

--- lib/evo/benchmark.ex
+++ lib/evo/benchmark.ex
@@ -2,13 +2,31 @@ defmodule Evo.Benchmark do
 	def measure_execution_time(func) when func |> is_function do
 		{µs, _} = :timer.tc(fn -> func.() end)
 
-		[
-			µs: Aja.Integer.decimal_format(µs),
-			ms: round(µs / 1000) |> Aja.Integer.decimal_format(),
-			s: Float.round(µs / 1_000_000, 2) |> Float.to_string
-		]
+		IO.puts(
+		"""
+
+			µs: #{Aja.Integer.decimal_format(µs) <> "µs"}
+			ms: #{handle_ms_conversion(µs)}
+			 s: #{handle_sec_conversion(µs)}
+		"""
+		)
 	end
 
 	def measure_execution_time(_), do:
 		raise "Must pass function in, you probably passed a value by accident"
+
+	defp handle_ms_conversion(µs) do
+		cond do
+			µs < 1_000 -> "< 1"
+			true -> round(µs / 1_000) |> Aja.Integer.decimal_format()
+		end <> "ms"
+	end
+
+	defp handle_sec_conversion(µs) do
+		if µs < 100_000 do
+			"≈ 0"
+		else
+			Float.round(µs / 1_000_000, 2) |> Float.to_string
+		end <> "s"
+	end
 end
